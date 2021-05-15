51aeeba37add742989808dbaac89dddd1ecb6652
lipeoliveira <felipecardosodeoliveira02@gmail.com>
2021-05-14 22:07:05 -0300

    iniciando projeto e configurando express

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 var http = require('http');
+var app = require('./config/express')();
 
-http.createServer(function (req, res) {
-    res.writeHead(200, {
-        'Content-Type': 'text/plain'
-    });
-    res.end('sou um servidor node')
-}).listen(3000, '127.0.0.1');
+http.createServer(app).listen(app.get('port'), function () {
+    console.log('Express rodando na porta ', app.get('port'));
+});
