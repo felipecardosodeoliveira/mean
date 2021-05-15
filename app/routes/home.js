e63a4fbd2f131071fcb5a5443121a30f7756103d
lipeoliveira <felipecardosodeoliveira02@gmail.com>
2021-05-14 23:25:51 -0300

    criando controler para a home e ligando à rota

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,3 +1,6 @@
+var controller = require('../controllers/home')();
+
 module.exports = function (app) {
-    app.get('/');
+    app.get('/index', controller.index);
+    app.get('/', controller.index);
 }
