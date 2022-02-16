c65ba37e44fdb6cedeae2ee126ad185e52a4bd33
lipeoliveira <felipecardosodeoliveira02@gmail.com>
2022-02-15 23:33:24 -0300

    Removido template engine do express e adicionado AngularJs no Aapp

---
+++ public/js/controllers/ContatosController.js
@@ -0,0 +1,12 @@
+angular.module('contatooh').controller('ContatosController', function ($scope) {
+    $scope.total = 0;
+
+    $scope.increment = function () {
+        $scope.total++;
+    }
+
+
+
+
+
+});
